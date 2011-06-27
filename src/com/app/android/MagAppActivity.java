a08597ccc3d949fe2478e2a200e5024814c9a59a
ksenia <ksenials@web.de>
2011-06-27 20:50:08 +0200

    Login successfull
    Change-Id: I322ab573dffb86ad81db1a931e3e0fd8eaae3c0d

--- src/com/app/android/MagAppActivity.java
+++ src/com/app/android/MagAppActivity.java
@@ -11,6 +11,7 @@ import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
